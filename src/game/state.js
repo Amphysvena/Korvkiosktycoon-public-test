43f95a5a8c82294632442b6130882a3ef787e812
Amphysvena <rubismail@yahoo.com>
2025-07-21 14:16:29 +0200

    Initial commit

---
+++ src/game/state.js
@@ -0,0 +1,3 @@
+import Decimal from 'break_infinity.js'; // imports the Decimal object from the break_infinity package. It can handle numbers bigger in magnitude than 1e308 
+
+let sausages = new Decimal(0); // Number of sausages
